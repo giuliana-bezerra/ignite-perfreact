a703bc58516b247de2f45a6036a1d06bdf496528
Giuliana Silva Bezerra <giu.drawer@gmail.com>
2021-10-25 19:55:46 -0300

    add memo

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,3 +1,14 @@
+// O memo evita que seja criada uma nova versão do componente quando nenhuma propriedade dele for alterada.
+// O memo faz uma shallow compare -> comparação rasa das propriedades do componente, e no javascript é checada igualdade referencial entre objetos.
+// Por isso precisa ser passado um segundo parâmetro para fazer a checagem de igualdade correta.
+
+// Usar o memo:
+// 1. Para pure funcional components (dados os mesmos parâmetros, é retornado o mesmo resultado)
+// 2. Componentes que renderizam demais
+// 3. Rerender com as mesmas props
+// 4. Componentes médios ou grandes performam melhor com o memo
+import { memo } from 'react';
+
 interface ProductItemprops {
   product: {
     id: number;
@@ -6,10 +17,18 @@ interface ProductItemprops {
   };
 }
 
-export function ProductItem({ product }: ProductItemprops) {
+function ProductItemComponent({ product }: ProductItemprops) {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
     </div>
   );
 }
+
+export const ProductItem = memo(
+  ProductItemComponent,
+  (prevProps, nextProps) => {
+    // Deep compare
+    return Object.is(prevProps.product, nextProps.product);
+  }
+);
